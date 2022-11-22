0684fb58300a8b000789ba429a7963e481dfcf8b
Steve Crow <steve@smcrow.com>
2022-11-22 16:36:06 -0500

    Use a union to make sort order of results better.

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -11,13 +11,21 @@ export async function GET(request: RequestEvent) {
 		return json([]);
 	}
 
+	// Search for a matching identifier first and then union with a search against the names.
+	// This helps prioritize results since most people searching for an identifier will probably want those first.
 	const [rows] = await connection.promise().query(
 		`
 		SELECT d.identifier, d.kind, d.city, d.state, d.country, d.latitude, d.longitude, IFNULL(d.name, n.name) as name
 		FROM
 			data d
 			LEFT JOIN names n ON n.identifier = d.identifier
-		WHERE d.identifier = ? or IFNULL(d.name, n.name) like ?
+		WHERE d.identifier = ?
+		UNION
+		SELECT d.identifier, d.kind, d.city, d.state, d.country, d.latitude, d.longitude, IFNULL(d.name, n.name) as name
+		FROM
+			data d
+			LEFT JOIN names n ON n.identifier = d.identifier
+		WHERE IFNULL(d.name, n.name) LIKE ?
 		`,
 		[query, `${query}%`]
 	);
