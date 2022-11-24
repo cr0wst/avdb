274aa9e6b5ad438ffc38cf5863750bfe799534f0
Steve Crow <steve@smcrow.com>
2022-11-24 13:57:55 -0500

    Dynamic chart type depending on waypoint.

--- src/components/ChartImage.svelte
+++ src/components/ChartImage.svelte
@@ -4,10 +4,35 @@
 	export let fix;
 	let script;
 
+	// 301 = VFR, 302 = World Low, 304 = World High
+	const chartMap = {
+		AIRPORT: 301,
+		WP: 304,
+		RP: 302,
+		MW: 301,
+		MR: 301,
+		CN: 301,
+		RADAR: 301,
+		NRS: 304,
+		VFR: 301,
+		NDB: 301,
+		VORTAC: 302,
+		VOT: 302,
+		'VOR/DME': 302,
+		'NDB/DME': 302,
+		VOR: 302,
+		TACAN: 302,
+		DME: 302,
+		'FAN MARKER': 301,
+		'MARINE NDB': 301
+	};
+
 	afterUpdate(() => {
 		script = document.createElement('script');
 		document.getElementById('sv_' + fix.id).appendChild(script);
-		script.src = `//skyvector.com/api/lchart?ll=${fix.latitude},${fix.longitude}&amp;s=1&amp;c=sv_${fix.id}&amp;i=301`;
+		script.src = `//skyvector.com/api/lchart?ll=${fix.latitude},${fix.longitude}&amp;s=1&amp;c=sv_${
+			fix.id
+		}&amp;i=${chartMap[fix.kind] || 301}`;
 	});
 </script>
 
