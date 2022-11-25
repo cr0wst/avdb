d544738181bafd2603ea08f7e8e1310c5aaa0d4c
Steve Crow <steve@smcrow.com>
2022-11-24 20:11:20 -0500

    Add meta properties for unfurling.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,14 @@
 	import Header from '../components/Header.svelte';
 </script>
 
-<svelte:head><title>vavDB - The Virtual Aviation Database</title></svelte:head>
+<svelte:head>
+	<title>vavDB - The Virtual Aviation Database</title>
+	<meta property="og:title" content="vavDB.com" />
+	<meta
+		property="og:description"
+		content="Welcome to vavDB. This website was created to help quickly look up airports and other identifiers while flying on the VATSIM network. The information is sourced using the FAA Aeronotical Data." />
+	<meta property="og:url" content="https://vavdb.com" />
+</svelte:head>
 <div class="min-h-screen flex flex-col">
 	<Header />
 	<div class="w-full lg:w-5/6 mx-auto mt-2">
